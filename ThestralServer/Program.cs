6e418102b0bb359526b7dd51fe2559d71a764ca4
Ampderg <guywithamp@hotmail.com>
2021-09-01 16:49:30 -0700

    Initial Commit

---
+++ ThestralServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThestralServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
